3c46c5694342d4f0b1d1b8eff3cee0571150a30c
Mike Lukina <191742789+mike-run@users.noreply.github.com>
2024-12-21 21:05:34 +0000

    feat: implement program that takes input and changes it to lowercase

---
+++ python/cs50-python/week-0-functions-and-variables/problem-set-0/indoor.py
@@ -0,0 +1,8 @@
+# This program prompts the user for input and then outputs the same input in
+# lowercase
+
+def main():
+    user_input = input("Please enter some text: ").lower()
+    print(user_input)
+
+main()
