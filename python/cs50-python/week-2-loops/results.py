7850777e3b1d3c9242b2b99002777f746e17412c
Mike Lukina <191742789+mike-run@users.noreply.github.com>
2024-12-24 21:00:08 +0000

    feat: add program that demonstrates how to manipulate lists using different methods

---
+++ python/cs50-python/week-2-loops/results.py
@@ -0,0 +1,22 @@
+# This program demonstrates how to manipulate lists using different methods
+
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Koopa Troopa", "Toad", 
+           "Bowser", "Donkey Kong Jr."
+]
+
+results.remove("Bowser")
+results.insert(0, "Bowser")
+results.reverse()
+
+# results = ["Mario", "Luigi"]
+
+# results.append("Princess")
+# results.append("Yoshi")
+# results.append("Koopa Troopa")
+# results.append("Toad")
+
+# results.append(["Bowser", "Donkey Kong Jr."])
+# results.remove(["Bowser", "Donkey Kong Jr."])
+# results.extend(["Bowser", "Donkey Kong Jr."])
+
+print(results)
