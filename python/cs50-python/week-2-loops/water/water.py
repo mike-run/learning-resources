51290852ac65a14c9f89b2d0b6ad19be0ed78168
Mike Lukina <191742789+mike-run@users.noreply.github.com>
2024-12-24 01:41:29 +0000

    feat: add number of days it took to reach a moisture level of below 20%

--- python/cs50-python/week-2-loops/water/water.py
+++ python/cs50-python/week-2-loops/water/water.py
@@ -5,11 +5,13 @@ def main():
     # Gets an initial moisture reading so that the while loop further below has
     #  a value to check against
     moisture = sample()
-    print(f"Moisture is {moisture}%")
+    days = 0
+    print(f"Day {days}: Moisture is {moisture}%")
 
     while moisture > 20:
         moisture = sample()
-        print(f"Moisture is {moisture}%")
+        days += 1
+        print(f"Day {days}: Moisture is {moisture}%")
 
     print("Time to water!")
 
